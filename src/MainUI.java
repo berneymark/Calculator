32ee0dfbbb36591dc424e6002378d9a498b0f2ce
Mark Berney <berneydwmark@gmail.com>
2019-04-16 09:40:24 +1200

    Initial commit

---
+++ src/MainUI.java
@@ -0,0 +1,18 @@
+/*
+ * @author: Mark Berney
+ * @version: 1.0
+ */
+
+public class MainUI {
+    public MainUI() {
+        GUIinit();
+    }
+
+    public void GUIinit() {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
