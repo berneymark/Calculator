43bacf629d56c401b2a8c47d8d631d6048a685e0
Mark Berney <berneydwmark@gmail.com>
2019-04-17 09:10:01 +1200

    update init

--- src/MainUI.java
+++ src/MainUI.java
@@ -3,16 +3,27 @@
  * @version: 1.0
  */
 
+import javax.swing.*;
+import java.awt.*;
+
 public class MainUI {
     public MainUI() {
-        GUIinit();
+        GUI();
     }
 
-    public void GUIinit() {
-        
+    public void GUI() {
+        JFrame frame = new JFrame("Calculator");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        frame.setContentPane(mainPanel);
+
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-
+        MainUI main = new MainUI();
     }
 }
